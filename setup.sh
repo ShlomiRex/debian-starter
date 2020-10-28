9257e8de0149c763ae7b35d5f561991e78ea3d29
Shlomi Domnenko <shlomidomnenko@pop-os.localdomain>
2020-10-29 00:38:59 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ sudo apt upgrade
 
 #install stuff
 sudo apt update
-sudo apt install wget python3 python3-pip wine winetricks zsh xterm openssl gdebi steam code torbrowser-launcher wireshark-qt htop vlc cmake openjdk-14-jre openjdk-14-jdk neofetch vim curl docker docker-compose virtualbox spotify-client blender krita gimp openshot audacity timeshift stacer
+sudo apt install wget python3 python3-pip wine winetricks zsh xterm openssl gdebi steam code torbrowser-launcher wireshark-qt htop vlc cmake openjdk-14-jre openjdk-14-jdk neofetch vim curl docker docker-compose virtualbox spotify-client blender krita gimp openshot audacity timeshift stacer gnome-tweaks snapd cifs-utils
+
+#snap packages
+snap install spotify
 
 #gpu nvidia drivers
 sudo ubuntu-drivers devices
 sudo ubuntu-drivers autoinstall
-
-#rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 #zoom
 wget https://zoom.us/client/latest/zoom_amd64.deb
@@ -31,4 +31,13 @@ cp balenaEtcher-1.5.109-x64.AppImage ~/Desktop
 #google-chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+
+#rust
+read -p "Install rust language? [y/N] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
 
